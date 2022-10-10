f51ba2586e4a92c950ae4855c0f755b5594ccde9
Anderson Tavares <31394163+AndersonHTavares@users.noreply.github.com>
2022-10-10 16:46:16 -0300

    Credentials Removed

--- providers.tf
+++ providers.tf
@@ -10,8 +10,6 @@ terraform {
 # Configura o Provider Google Cloud com o Projeto
 provider "google" {
 
-  credentials = file("/home/andersonrhtlab/gcpkey.json")
-
   project = "labdevopscloud343515"
   region  = "us-west1"
   zone    = "us-west1-b"
